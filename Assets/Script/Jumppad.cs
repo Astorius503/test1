566af44e185f2bfb51aaf12cec2cb1366ad2b173
Astorius503 <tiggh12@gmail.com>
2025-03-11 01:02:29 +0900

    [chore]점프패드 못 만들겠다
    머리가 안 굴러가네.하나 고치면 난리고 하나 고치면 난리고 에라 모르겠다.

--- Assets/Script/Jumppad.cs
+++ Assets/Script/Jumppad.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Jumppad : MonoBehaviour
+public class JumpPad : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float jumpForce = 15f; // 점프 힘 조절 가능
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        // 플레이어가 점프대를 밟았는지 확인
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("점프패드 접촉");
+
+            Rigidbody playerRb = other.GetComponent<Rigidbody>();
+            if (playerRb != null)
+            {
+                Debug.Log("점프패드 활성화");
+                // 현재 속도를 고려하여 Y축으로 힘 가하기
+                playerRb.velocity = new Vector3(playerRb.velocity.x, 0f, playerRb.velocity.z);
+                playerRb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+            }
+        }
     }
 }
+
